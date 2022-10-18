3c432af64aeb5f10783cafd54487da5b888d9fa1
celsiusoft <wilfredo.machado@hotmail.com>
2022-10-17 21:59:02 -0400

    update changes 4 /me

--- src/users/users.services.js
+++ src/users/users.services.js
@@ -1,8 +1,6 @@
 const usersControllers = require("./users.controllers");
-
 const getAllUsers = (req, res) => {
-  usersControllers
-    .getAllUsers()
+  usersControllers.getAllUsers()
     .then((data) => {
       res.status(200).json(data);
     })
@@ -13,8 +11,7 @@ const getAllUsers = (req, res) => {
 
 const getUserById = (req, res) => {
   const id = req.params.id;
-  usersControllers
-    .getUserById(id)
+  usersControllers.getUserById(id)
     .then((data) => {
       res.status(200).json(data);
     })
@@ -25,7 +22,6 @@ const getUserById = (req, res) => {
 
 const registerUser = (req, res) => {
   const {firstName, lastName, email, password, phone, birthday, gender, country } = req.body;
-
     if (
         firstName &&
         lastName &&
@@ -34,35 +30,31 @@ const registerUser = (req, res) => {
         phone &&
         birthday
     ) {
-        //? Ejecutamos el controller
-        usersControllers.createUser({
-            firstName, lastName, email, password, phone, birthday, gender, country
-        })
-            .then( data => {
-                res.status(201).json(data)
-            })
-            .catch(err => {
-                res.status(400).json(err.message)
-            })
+    //? Ejecutamos el controller
+    usersControllers.createUser({firstName,lastName,email,password,phone,birthday, gender, country})
+    .then( data => {
+        res.status(201).json(data)
+    })
+    .catch(err => {
+        res.status(400).json(err.message)
+    })
     } else {
     //? Error cuando no mandan todos los datos necesarios para crear un usuario
-        res.status(400).json({message: 'All fields must be completed', fields: {
-            firstName: 'string',
-            lastName: 'string',
-            email: 'example@example.com',
-            password: 'string',
-            phone: '+521231231231',
-            birthday: 'YYYY/MM/DD'
-        }})
-    }
+    res.status(400).json({message: 'All fields must be completed', fields: {
+        firstName: 'string',
+        lastName: 'string',
+        email: 'example@example.com',
+        password: 'string',
+        phone: '+521231231231',
+        birthday: 'YYYY/MM/DD'
+    }})
+  }
 };
 
 const patchUser = (req, res) => {
   const id = req.params.id;
   const { firstName, lastName, phone, gender, country } = req.body;
-
-  usersControllers
-    .updateUser(id, { firstName, lastName, phone, gender, country })
+  usersControllers.updateUser(id, { firstName, lastName, phone, gender, country })
     .then((data) => {
       if (data[0]) {
         res
@@ -79,8 +71,7 @@ const patchUser = (req, res) => {
 
 const deleteUser = (req, res) => {
   const id = req.params.id;
-  usersControllers
-    .deleteUser(id)
+  usersControllers.deleteUser(id)
     .then((data) => {
       if (data) {
         res.status(204).json();
@@ -107,7 +98,6 @@ const getMyUser = (req , res) => {
 
 const deleteMyUser = (req , res) => {
   const id = req.user.id //? req.user contiene la informacion del token desencriptado
-
   usersControllers.deleteUserById(id)
     .then(data => {
         res.status(204).json(data)
